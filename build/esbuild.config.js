9d7fa53d512b4b47ffef69f685229a76d03c7e57
Keven Chionh <chionhkw@gmail.com>
2026-08-25 00:35:43 +0800

    Try to fix problems building with --platform=browser

---
+++ build/esbuild.config.js
@@ -0,0 +1,19 @@
+import { build } from 'esbuild';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+
+build({
+    entryPoints: ['index.js'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    platform: 'browser', // Tell esbuild this runs in a web browser!
+    target: ['es2022'],
+    outfile: 'dist/bundle.js',
+    plugins: [
+        NodeModulesPolyfillPlugin() // Polyfills Node's crypto, stream, buffers etc., for teleproto
+    ],
+    define: {
+        'process.env.NODE_ENV': '"production"',
+        'global': 'window', // Maps Node's global object to the browser window
+    }
+}).catch(() => process.exit(1));
